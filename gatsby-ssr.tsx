e5eb614ad62b0fb1fb0036c8c5323727ed1593f8
Florian Vanthuyne <hello@flovan.be>
2024-10-10 10:35:38 +0200

    feat: add umami tracking script

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -1,4 +1,4 @@
-import { RenderBodyArgs } from 'gatsby'
+import { RenderBodyArgs, Script } from 'gatsby'
 
 export const onRenderBody = ({ setHeadComponents }: RenderBodyArgs) => {
   setHeadComponents([
@@ -15,6 +15,11 @@ export const onRenderBody = ({ setHeadComponents }: RenderBodyArgs) => {
       as="image"
       crossOrigin="anonymous"
       key="svgSprite"
+    />,
+    <Script
+      src="https://cloud.umami.is/script.js"
+      data-website-id="6023396a-5ef0-4d77-8a57-50330d3af849"
+      key="umamiAnalytics"
     />,
   ])
 }
