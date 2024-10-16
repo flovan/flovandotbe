628c4344fab7e3ef0514d1eb4bf78de61eecef5e
Florian Vanthuyne <hello@flovan.be>
2024-10-16 22:30:59 +0200

    feat: improve overflow hiding

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -16,7 +16,7 @@ const Layout = ({ children, footerComponent }: LayoutProps) => {
   }, [language])
 
   return (
-    <div className="flex min-h-screen max-w-[100vw] flex-col items-stretch pt-flovan-sm max-sm:overflow-x-hidden">
+    <div className="flex min-h-screen max-w-[100vw] flex-col items-stretch overflow-x-clip pt-flovan-sm">
       <Header className="relative z-40 px-flovan-sm md:px-flovan-base lg:px-flovan-md" />
       <main className="relative z-20 -mb-flovan-sm flex flex-1 flex-col gap-flovan-lg rounded-bl-default rounded-br-default bg-background px-flovan-sm pb-flovan-md pt-flovan-base sm:pt-flovan-md md:px-flovan-base md:pb-flovan-lg lg:px-flovan-md xl:gap-flovan-xl">
         {children}
